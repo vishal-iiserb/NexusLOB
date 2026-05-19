bd6d0564c2309a2bb68771968596e1c9a24481fa
vishal-iiserb <vishal.iiserb25@gmail.com>
2026-05-20 04:58:01 +0530

    Integrated Stable-Baselines3, wrote training script, and successfully trained first PPO neural network

---
+++ train_ai.py
@@ -0,0 +1,18 @@
+from stable_baselines3 import PPO
+from nexus_env import NexusTradingEnv
+import os
+os.add_dll_directory(r"C:/mingw64/bin") 
+
+print("Booting up the trading environment...")
+env = NexusTradingEnv()
+
+print("Attaching the PPO brain...")
+model = PPO("MlpPolicy", env, verbose=1)
+
+print("Starting the training montage... (10,000 steps)")
+model.learn(total_timesteps=10000)
+
+print("Training finished! Saving the data to disk...")
+model.save("nexus_quant_brain")
+
+print("SUCCESS: Brain saved as 'nexus_quant_brain.zip'!")
